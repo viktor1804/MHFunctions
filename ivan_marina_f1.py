b0774c559d7102a09a45d7262d2faaff8fec248b
viktor1804 <44602972+viktor1804@users.noreply.github.com>
2018-11-10 19:12:09 +0200

    ivan and marina

---
+++ ivan_marina_f1.py
@@ -0,0 +1,15 @@
+def func(x):
+    return x*3+2
+
+def kick(l):
+    if l<5:
+        return True
+    else:
+        return False
+    
+def even(s):
+    if s%2==0:
+        return True
+    else:
+        return False
+
