190c86c74ad3a0f85a4e6c1ad7454e43f84f0cda
viktor1804 <44602972+viktor1804@users.noreply.github.com>
2018-11-10 19:16:16 +0200

    Add files via upload

---
+++ boris_georgi_f1.py
@@ -0,0 +1,17 @@
+def even(x):
+    if x%2==0:
+	    return "chisloto e chetno"
+    else:
+	    return "chislto e nechetno"
+
+
+def kick(l):
+    if l>=5:
+	    return False
+    else:
+	    return True
+
+
+
+def func(x):
+	return x*3+2
